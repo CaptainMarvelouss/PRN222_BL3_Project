ba3a053bb0099ae73ce33decb2969afa8d849817
sdang <sdang6666@gmail.com>
2025-04-17 16:42:10 +0700

    crud football fields

---
+++ Repositories/FootballFieldRepository.cs
@@ -0,0 +1,23 @@
+﻿using BusinessObjects;
+using DataAccessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class FootballFieldRepository : IFootballFieldRepository
+    {
+        public List<Field> GetFootballFields() => FootballFieldDAO.GetFootballFields();
+
+        public Field GetFootballFieldById(int id) => FootballFieldDAO.GetFootballFieldById(id);
+
+        public void AddFootballField(Field field) => FootballFieldDAO.AddFootballField(field);
+
+        public void UpdateFootballField(Field field) => FootballFieldDAO.UpdateFootballField(field);
+
+        public void DeleteFootballField(Field field) => FootballFieldDAO.DeleteFootballField(field);
+    }
+}
